4b38c6ee12b24e0f0246802ad50f8fab1fec9427
Fabian Cook <hello@fabiancook.dev>
2019-08-24 13:33:39 +1200

    Expose QuadFind

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { DatasetCoreFactoryImplementation } from "./dataset-core-factory-implementation";
 
+export { QuadFind } from "./dataset-core";
 export { isMatch } from "./match";
 
 export const DatasetFactory = new DatasetCoreFactoryImplementation();
