398403810e79347d6923e77aa1ea39d2e67c7611
Fabian Cook <hello@fabiancook.dev>
2019-08-24 14:08:06 +1200

    Expose interfaces

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,13 @@ import { DatasetCoreFactoryImplementation } from "./dataset-core-factory-impleme
 
 export { QuadFind } from "./dataset-core";
 export { isMatch } from "./match";
+export { DatasetCore } from "./dataset-core";
+export { Dataset } from "./dataset";
+export { DatasetCoreFactory } from "./dataset-core-factory";
+export * from "./quad-filter-iteratee";
+export * from "./quad-map-iteratee";
+export * from "./quad-reduce-iteratee";
+export * from "./quad-run-iteratee";
 
 export const DatasetFactory = new DatasetCoreFactoryImplementation();
 
